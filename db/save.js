215fa76d06ddc20619aa623ebd345bd227facc94
breakfireaus <myoungerc@hotmail.com>
2022-10-25 13:19:42 +1100

    updated save.js fixed bug

--- db/save.js
+++ db/save.js
@@ -1,5 +1,5 @@
-const util = require('util');
-const fs = require('fs');
+const util = require("util");
+const fs = require("fs");
 
 //use node.js util function to promisify read and write with POSTs, Gets and DELETE
 const readFileAsync = util.promisify(fs.readFile);
@@ -7,40 +7,40 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 //creating a constructor class
 class Store {
-  constructor() {
-    this.lastId = 0;
-  }
-  //method to interpret object data in the db.json
-  read() {
-    return readFileAsync('db/db.json', 'utf8');
-  }
+    constructor(lastId) {
+      this.lastId = 0;
+    }
+//method to interpret object data in the db.json
+    read() {
+        return readFileAsync("db/db.json", "utf8");
+    }
 
-  // write object data(note) into db.json file also convert into string
-  write(note) {
-    return writeFileAsync('db/db.json', JSON.stringify(note));
-  }
+    // write object data(note) into db.json file also convert into string
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note));
+    }
 
-  // GET method
-  getTheNotes() {
-    return this.read().then((notes) => {
-      let parsedNotes;
-      try {
-        parsedNotes = [].concat(JSON.parse(notes));
-      } catch (err) {
-        parsedNotes = [];
-      }
-      // prints note
-      return parsedNotes;
-    });
-  }
-  // POST method
-  addTheNote(note) {
+    // GET method
+    getTheNotes(){
+        return this.read().then((notes) => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+              } catch (err) {
+                parsedNotes = [];
+              }
+              // prints note
+              return parsedNotes;
+            });
+          }
+ // POST method
+ addTheNotes(note) {
     const { title, text } = note;
 
     if (!title || !text) {
       throw new Error("Note 'title' and 'text' cannot be blank");
     }
-    const newNote = { title, text, id: ++this.lastId };
+    const newNote = { title, text, id: this.lastId++ };
     return this.getTheNotes()
       .then((notes) => [...notes, newNote])
       .then((updatedNotes) => this.write(updatedNotes))
@@ -48,10 +48,10 @@ class Store {
   }
 
   // DELETE note method
-  removeNote(id) {
+  deleteNote(id) {
     return this.getTheNotes()
-      .then((notes) => notes.filter((note) => note.id !== parseInt(id)))
-      .then((filteredNotes) => this.write(filteredNotes));
+    .then((notes) => notes.filter((note) => note.id !== parseInt(id)))
+    .then((filteredNotes) => this.write(filteredNotes));
   }
 }
 
